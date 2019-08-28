836e7df57a0694bfcd6b35108a3df4d304d37c7c
Lorenzo Simpson <lorenzosimp@gmail.com>
2019-08-28 17:03:13 -0400

    first commit, create form and styling

---
+++ js/main.js
@@ -0,0 +1,11 @@
+function milesOrMoney(miles, money, value) {
+    let cpm = (money / miles);
+    if (cpm * 100 >= value) {
+        return `Use your miles!`;
+    }
+    else {
+        return `Use cash!`;
+    }
+}
+
+
